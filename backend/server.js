ff8939c0c3e90dd1b14571c155ae7ac03078c473
Alaiah Garcia - Mallari <PhilippeMallari>
2023-10-02 18:26:45 -0600

    added routes and controllers for user auth

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send("This is the blog post API")
 })
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
